4a880a61d7bba3e8d5cf458bee162a6fe1356663
tothrichard <32360900+tothrichard@users.noreply.github.com>
2019-12-08 19:14:31 +0100

    rules & test

---
+++ rules/OLVAS/OLVAS/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+
+namespace OLVAS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader olvas = new StreamReader("RULES.txt", Encoding.Default);
+            string szoveg;
+            while (!olvas.EndOfStream)
+            {
+                szoveg = olvas.ReadLine();
+                Console.WriteLine(szoveg);
+            }
+           
+            olvas.Close();
+            Console.ReadLine();
+        }
+    }
+}
